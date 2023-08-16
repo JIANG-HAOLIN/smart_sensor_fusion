9c29e679a792d066bec15bd7d1614876a9c9492c
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2023-08-16 13:47:50 +0200

    added the scripts for inference, override the nn.TransformerEncoderLayer to output the att_weights, and added hook to get the att_weights. also added visualization for att_weights

---
+++ utils/plot_attn.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_attention_maps(input_data, attn_maps, idx=0) -> None:
+    """Plot the attention map
+
+    Args:
+        input_data: should be None or [batch_size, seq_len]
+        attn_maps: should have size [num_laer, batch_size, num_head, seq_len, model_dim]
+        idx: batch index (which sample of a batch)
+
+    """
+    if input_data is not None:
+        input_data = input_data[idx].detach().cpu().numpy()
+    else:
+        input_data = np.arange(attn_maps[0][idx].shape[-1])
+    attn_maps = [m[idx].detach().cpu().numpy() for m in attn_maps]
+
+    num_heads = attn_maps[0].shape[0]
+    num_layers = len(attn_maps)
+    seq_len = input_data.shape[0]
+    fig_size = 4 if num_heads == 1 else 3
+    fig, ax = plt.subplots(num_layers, num_heads, figsize=(num_heads * fig_size, num_layers * fig_size))
+    if num_layers == 1:
+        ax = [ax]
+    if num_heads == 1:
+        ax = [[a] for a in ax]
+    for row in range(num_layers):
+        for column in range(num_heads):
+            ax[row][column].imshow(attn_maps[row][column], origin='lower', vmin=0)
+            ax[row][column].set_xticks(list(range(seq_len)))
+            ax[row][column].set_xticklabels(input_data.tolist())
+            ax[row][column].set_yticks(list(range(seq_len)))
+            ax[row][column].set_yticklabels(input_data.tolist())
+            ax[row][column].set_title(f"Layer {row + 1}, Head {column + 1}")
+    fig.subplots_adjust(hspace=0.5)
+    plt.show()
