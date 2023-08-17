1ce4eab7494388812f6b70e916fe6367bb00e373
Andras Kupcsik <andrasgabor.kupcsik@de.bosch.com>
2023-08-17 11:34:25 +0200

    updated req and added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='smart_sensor_fusion',
+      version='0.1',
+      description='Smart sensor fusion with transformers package',
+      python_requires='>=3.9',
+     )
