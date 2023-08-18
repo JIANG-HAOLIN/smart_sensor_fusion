2354b093dc26811037125a99ec30fb5a1b63a68e
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2023-08-18 12:01:37 +0200

    add argparser for demo file, and also changed the way checkpoints loaded, now only the nn.Module part will be loaded from checkpoints

--- scripts/demo_transpose_numbers.py
+++ scripts/demo_transpose_numbers.py
@@ -1,40 +1,63 @@
 import os
+import sys
 import torch
-from src.pl_modules.tutorial_6_transpose import TransformerPredictorPl
+import argparse
+import numpy as np
+import hydra
+from hydra import compose, initialize
+from omegaconf import DictConfig, OmegaConf
 
-from utils.plot_attn import plot_attention_maps
 
-
-def inference(test_seq: torch.Tensor,
-              ckpt_path: str) -> None:
+def inference(cfg: DictConfig, args: argparse.Namespace) -> None:
     """
 
     Args:
-        test_seq: input test sequence with shape [batch size, 17]
-        ckpt_path: absolution path of checkpoint
+        cfg: hydra config file
+        args: the input arguments
 
     Output the prediction sequence and visualization of the attention map
 
     """
+    from utils.plot_attn import plot_attention_maps
+    test_seq = torch.randint(10, size=(1, 17), device='cuda') if args.test_seq is None \
+        else torch.from_numpy(np.array(args.test_seq)).unsqueeze(0).to('cuda')
     torch.set_float32_matmul_precision('medium')
     batch_input = torch.nn.functional.one_hot(test_seq, num_classes=10).float()
-    if os.path.isfile(ckpt_path):
+    if os.path.isfile(args.ckpt_path):
         print("Found pretrained model, loading...")
-        pretrained_mdl = TransformerPredictorPl.load_from_checkpoint(ckpt_path)
+        # pretrained_mdl = TransformerPredictorPl.load_from_checkpoint(args.ckpt_path) # if we load the whole lightningModule
+        pretrained_mdl = torch.load(args.ckpt_path)['hyper_parameters']['mdl']
         pretrained_mdl.eval()
-        pretrained_mdl.freeze()
-        out = pretrained_mdl.mdl(batch_input)
+        # pretrained_mdl.freeze()  # only for lightningModule
+        # out = pretrained_mdl.mdl(batch_input)  # only for lightningModule
+        out = pretrained_mdl(batch_input)
+        print('Done loading')
         attn_maps = out[1]
-        print('test label:', torch.flip(test_seq, dims=(1,)), '\n',
-              'output prediction:', out[0].argmax(-1), '\n',)
+        print('input sequence', test_seq, '\n',
+              'test label:', torch.flip(test_seq, dims=(1,)), '\n',
+              'output prediction:', out[0].argmax(-1), '\n', )
         attn_maps = torch.stack(attn_maps, dim=0)
         plot_attention_maps(test_seq, attn_maps, idx=0)
-        print('Done loading')
 
 
 if __name__ == "__main__":
-    test_seq = torch.randint(10, size=(1, 17), device='cuda')
-    inference(test_seq,
-              '/home/jin4rng/Documents/code/smart_sensor_fusion/results/simple_transformer_multiblocks/logits/transpose08-17-15:28:27test/checkpoints/08-17-15:28:28-jobid=0-epoch=2-step=1170.ckpt')
+    project_path = os.path.abspath(os.path.join(__file__, '..', '..'))
+    sys.path.append(project_path)  # so we can import the modules inside the project when running in terminal
+    print(project_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt_path', type=str,
+                        default='/home/jin4rng/Documents/code/smart_sensor_fusion/results'
+                                '/simple_transformer_multiblocks/logits/transpose08-18-11:58:20test/checkpoints/08-18'
+                                '-11:58:21-jobid=0-epoch=1-step=780.ckpt',
+                        help="the path of pretrained .ckpt model, should have shape "
+                             "like:smart_sensor_fusion/results/model name/dataset name/task "
+                             "name+time stamp+test/checkpoints/...ckpt")
+    parser.add_argument('--test_seq', type=int, nargs='+', help='input test sequence, should be a list, e.g. '
+                                                                '--test_seq 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7')
+    args = parser.parse_args()
 
+    initialize(version_base=None, config_path="../configs", job_name="test_app")
+    cfg = compose(config_name="config")
+    print(OmegaConf.to_yaml(cfg))
 
+    inference(cfg=cfg, args=args)
