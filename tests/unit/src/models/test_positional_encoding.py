a61f3d9bae6ec2ad7c2decf68b4826fdd3890a75
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2023-08-23 15:06:20 +0200

    reformulate the unit test files

---
+++ tests/unit/src/models/test_positional_encoding.py
@@ -0,0 +1,15 @@
+import unittest
+import torch
+from src.models.positional_encoding import StandardPositionalEncoding
+
+
+class TestAddNumbers(unittest.TestCase):
+
+    def test_standard_postional_encoding(self):
+        pe = StandardPositionalEncoding()
+        input = torch.randn([2, 1000, 256])
+        self.assertEqual(pe(input).shape, torch.Size([2, 1000, 256]))
+
+
+if __name__ == '__main__':
+    unittest.main()
