3b8e75e456eb02b697f7451fda5c65ba3818187e
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2023-11-23 13:15:55 +0100

    added simple test for gpu

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -7,3 +7,10 @@ if torch.cuda.is_available():
     print(f"Cuda version:{torch.version.cuda}")
 else:
     print(f'no Cuda detected, using CPU instead !!')
+
+w = torch.nn.Parameter(torch.randn([1, ]), requires_grad=True)
+y = 1
+
+loss = y - w
+grad = loss.backward()
+print(w.grad)
