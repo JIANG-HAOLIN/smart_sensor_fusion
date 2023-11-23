900c03cba5641e443cb017f91d654812ab22c251
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2023-11-23 13:35:46 +0100

    add a simple pure pytorch based training pipeline to test if GPU is working correctly

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -1,4 +1,8 @@
+import math
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
 
 print(f"if Cuda available:{torch.cuda.is_available()}")
@@ -14,3 +18,44 @@ y = 1
 loss = y - w
 grad = loss.backward()
 print(w.grad)
+
+
+# Generate synthetic data
+torch.manual_seed(42)  # for reproducibility
+X = torch.linspace(0, 99, 100).reshape(-1, 1)
+y_true = torch.sin(X)
+y_noisy = y_true + torch.randn_like(y_true)
+
+# Define a simple neural network model
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.MLP = nn.Sequential(nn.Linear(1, 100), nn.ReLU(), nn.Linear(100, 10), nn.ReLU(), nn.Linear(10, 1))
+
+    def forward(self, x):
+        return self.MLP(x)
+
+# Instantiate the model, loss function, and optimizer
+model = SimpleModel()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 3
+
+for epoch in range(num_epochs):
+    # Forward pass
+    y_pred = model(X)
+
+    # Compute the loss
+    loss = criterion(y_pred, y_noisy)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Print the loss every 100 epochs
+    if (epoch + 1) % 1 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
