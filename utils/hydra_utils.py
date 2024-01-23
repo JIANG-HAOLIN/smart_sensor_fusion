fdb58eaaba6389cfb8522aac69b48b4213662638
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2024-01-23 17:34:14 +0100

    fixed bug

--- utils/hydra_utils.py
+++ utils/hydra_utils.py
@@ -5,9 +5,9 @@ from omegaconf import DictConfig, OmegaConf
 
 def extract_sweeper_output_label(hydra_conf: DictConfig, choices: DictConfig):
     labels = ''
-    for full_var_name in hydra_conf.variable_name:
+    for full_var_name in hydra_conf.variable_name.split('___'):
         conf = hydra_conf
-        for attr in full_var_name.split('___'):
+        for attr in full_var_name.split('.'):
             # hydra_conf = hydra_conf.__getattr__(attr)  # also works
             conf = conf[attr]
         if isinstance(conf, DictConfig):  # only works for trainers, pl_modules, optimizers, models, datasets
