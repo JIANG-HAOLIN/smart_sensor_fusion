2c19ff3094bdfdaeb0273b35386290b0c9954849
Jiang Haolin (CR/AIR4.1) <jin4rng@RNG-C-002N4.bf4.de.bosch.com>
2024-01-23 16:10:45 +0100

    add configuration for longer sequence

--- tests/unit/src/models/utils/test_mel_spec.py
+++ tests/unit/src/models/utils/test_mel_spec.py
@@ -6,9 +6,9 @@ import unittest
 class TestMelSpec(unittest.TestCase):
     def test_mel_spec(self):
         windows_size = 0.05
-        length = 40000
+        length = 120000
         sr = 16000
-        hop = 0.025
+        hop = 0.02
         n_mel = 64
         mel = MelSpec(windows_size=windows_size,
                       length=length,
