add my implementation for masking loss computation and fixed previous recovery mse loss bug

src/models/ssl_nce_framework.py
@@ -15,6 +15,7 @@ import hydra
 from typing import Optional, Dict, List
 from types import SimpleNamespace
 import time
+from src.models.utils.helpers import cosine_loss_fn, mse_fn
 
 
 class SslNceFramework_EarlySum(torch.nn.Module):
@@ -1118,7 +1119,8 @@ class SslNceFramework_EarlySum_VATT(torch.nn.Module):
                 recover_loss = self.mask_prediction(self.mask_latent_predictor,
                                                     mask_pred_target,
                                                     fused_t_masked_feats,
-                                                    masks=masked_index)
+                                                    masks=masked_index,
+                                                    loss=self.mask_latent_args.mask_latent_prediction.loss)
                 output["ssl_losses"]["recover_loss"] = recover_loss
 
             if "imitation" in task:
@@ -1389,17 +1391,34 @@ class SslNceFramework_EarlySum_VATT(torch.nn.Module):
     def mask_prediction(predictor: nn.Module,
                         target: torch.Tensor,
                         fused_t_mask_feats: torch.Tensor,
-                        masks: Optional[dict] = None):
+                        masks: Optional[dict] = None,
+                        loss: str = "mse",):
+        """
+
+        Args:
+            predictor:
+            target:
+            fused_t_mask_feats:
+            masks: shape [B, S ,1]
+            loss:
+
+        Returns:
+
+        """
         pred_out = predictor(fused_t_mask_feats)
+        if loss == "mse":
+            loss_fn = mse_fn
+        elif loss == "cosine":
+            loss_fn = cosine_loss_fn
 
         if masks is None:
-            pred_loss = F.mse_loss(pred_out, target)
+            pred_loss = loss_fn(pred_out, target)
         else:
+            # can not filter the zero value as number of masked ele varies alone dif dim
             mask = 1
             for mod_name, mode_mask in masks.items():
                 mask = mask * mode_mask
             mask = 1 - mask
-            pred_loss = mask * (pred_out - target) ** 2
-            pred_loss = torch.sum(pred_loss) / torch.sum(mask)
+            pred_loss = loss_fn(pred_out, target, mask)
 
         return pred_loss

src/models/utils/helpers.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 import numpy as np
-from typing import List
+from typing import List, Optional
 
 
 class MyPermute(nn.Module):
     """My class for permutation"""
+
     def __init__(self, index: List):
         super().__init__()
         self.index = index
@@ -144,3 +146,44 @@ def get_mask_sequence1d(seq_len: int, mask_prob: float = 0.15, mask_length: int 
         for idx in range(init_index, end_index):
             mask[idx] = mask_mark
     return mask
+
+
+def cosine_loss_fn(x, y, mask: Optional[torch.Tensor] = None):
+    """
+    my masked negative cosine similarity loss from BYOL(similar to SimSiam, shown as follows:
+                                                criterion = nn.CosineSimilarity(dim=1).cuda(args.gpu)
+                                                loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5)
+    Args:
+        x: input latent with shape [B, D] or [B, S, D]
+        y: target latent with shape [B, D] or [B, S, D]
+        mask: mask with shape [B, 1] or [B, S, 1]
+
+    Returns: similarity loss between x and y
+
+    """
+    x = F.normalize(x, dim=-1, p=2)
+    y = F.normalize(y, dim=-1, p=2)
+    if mask is None:
+        mask = torch.ones(x.shape[:-1]).unsqueeze(-1)
+    mask = mask.squeeze(-1)
+    pred_loss = mask * (2 - 2 * (x * y).sum(dim=-1))
+    pred_loss = torch.sum(pred_loss) / torch.sum(mask)
+    return pred_loss
+
+
+def mse_fn(x, y, mask: Optional[torch.Tensor] = None):
+    """
+    my masked mse loss fn
+    Args:
+        x: input latent with shape [B, D] or [B, S, D]
+        y: target latent with shape [B, D] or [B, S, D]
+        mask: mask with shape [B, 1] or [B, S, 1]
+
+    Returns: similarity loss between x and y
+
+    """
+    if mask is None:
+        mask = torch.ones(x.shape[:-1]).unsqueeze(-1)
+    pred_loss = mask * (x - y) ** 2
+    pred_loss = torch.sum(torch.mean(pred_loss, dim=-1)) / torch.sum(mask)  # mean value
+    return pred_loss